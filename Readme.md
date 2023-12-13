02c574c673e8c3ddabef5cc124fcc4f6cfafd238
JorgeUsatorre <usatorre1210@gmail.com>
2023-12-13 20:50:16 +0100

    plugin+bd

--- Readme.md
+++ Readme.md
@@ -8,5 +8,17 @@ Este plugin tiene como objetivo demostrar una funcionalidad simple de reemplazo 
 Reemplazo de Palabras Ofensivas: Sustituye palabras ofensivas en el contenido de las publicaciones por alternativas más neutrales.
 Configuración Personalizada: Permite personalizar la lista de palabras ofensivas y sus respectivos reemplazos.
 
+## Creación de Tabla en la Base de Datos
+El plugin crea automáticamente una tabla en la base de datos llamada wordpresswords. Esta tabla contiene columnas para el ID, la palabra ofensiva (insulto), y su equivalente más sutil (sutiles).
+
+## Inserción y Actualización de Datos
+El plugin inserta o actualiza automáticamente datos en la tabla wordpresswords. Se proporciona un conjunto inicial de datos que incluye palabras ofensivas y sus sustitutos más sutiles.
+
+## Reemplazo de Palabras Ofensivas
+Cada vez que se carga contenido (post o página), el plugin busca palabras ofensivas en la tabla wordpresswords y las reemplaza por sus equivalentes sutiles.
+
+## Instancias de Uso
+El plugin se activa mediante el gancho plugins_loaded, lo que garantiza que las funciones necesarias se ejecuten cuando se inicia WordPress. Además, utiliza el filtro the_content para realizar el reemplazo de palabras ofensivas.
+
 ## Configuración
 Puedes personalizar la lista de palabras ofensivas y sus reemplazos accediendo al código del plugin. Busca las variables $palabrasmalas y $palabrasbuenas en el archivo jorgeusatorre-words.php y realiza los ajustes necesarios.
--- plugin.php
+++ plugin.php
@@ -11,19 +11,55 @@ Author: JorgeUsatorre
 Version: 0.0.1
 */
 
-# List of offensive words
-$palabrasmalas= [
-    "retrasado",
-    "feo",
-    "fea",
-    "gilipollas"
-];
-$palabrasbuenas = [
-    "incompetente",
-    "guapo",
-    "guapa",
-    "paleto"
-];
+function inicioPlugin(){
+    createTable();
+    insertData();
+}
+/**
+ * Carga tabla wordpresswords con las palabras malsonantes
+ */
+function createTable(){
+    global $wpdb;
+    $table_name = 'wordpresswords';
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        insulto varchar(255) NOT NULL,
+        sutiles varchar(255) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+
+
+/**
+ * Inserta o actualiza datos en la tabla
+ */
+function insertData() {
+    global $wpdb;
+    $table_name = 'wordpresswords';
+    $data = array(
+        array('insulto' => 'retrasado', 'sutiles' => 'incompetente'),
+        array('insulto' => 'feo', 'sutiles' => 'guapo'),
+        array('insulto' => 'fea', 'sutiles' => 'guapa'),
+        array('insulto' => 'gilipollas', 'sutiles' => 'paleto'),
+    );
+
+    foreach ($data as $row) {
+        $existing_row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE insulto = %s", $row['insulto']));
+
+        if ($existing_row) {
+            // Si ya existe, actualiza el sutiles
+            $wpdb->update($table_name, array('sutiles' => $row['sutiles']), array('id' => $existing_row->id));
+        } else {
+            // Si no existe, inserta un nuevo registro
+            $wpdb->insert($table_name, $row);
+        }
+    }
+}
+
 
 /**
  * Whenever the word WordPress appears in the content
@@ -33,9 +69,16 @@ $palabrasbuenas = [
  * @return string
  */
 function jorgeusatorre_wordpress_typo_fix( $text ) {
-    global $palabrasmalas, $palabrasbuenas;
-    return str_replace( $palabrasmalas, $palabrasbuenas, $text );
+    global $wpdb;
+    $table_name = 'wordpresswords';
+    $results = $wpdb->get_results( "SELECT * FROM $table_name" );
+
+    foreach ($results as $row) {
+        $text = str_replace($row->insulto, $row->sutiles, $text);
+    }
+
+    return $text;
 }
 
-add_filter('the_content', 'jorgeusatorre_wordpress_typo_fix');
-
+add_action('plugins_loaded', 'inicioPlugin');
+add_filter('the_content', 'renym_wordpress_typo_fix');
